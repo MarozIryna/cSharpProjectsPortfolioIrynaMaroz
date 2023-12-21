b47edb034a687b954362333f2701d1c3aa208682
IrynaMaroz <maroz.iryna@gmail.com>
2023-12-21 17:33:34 +0200

    project 01 added

---
+++ project01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Iryna";
+            Console.WriteLine("Hello. My name is " + name);
+
+            var age = 30;
+            Console.WriteLine("My age is " + age);
+        }
+    }
+}
