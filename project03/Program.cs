c33830740589b5446f17a7874c1022d33f716f53
IrynaMaroz <maroz.iryna@gmail.com>
2023-12-22 10:47:11 +0200

    project03 added

---
+++ project03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace project03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string startLine = "---------------------";
+            string emptyLine = "|                   |";
+            string name = "Iryna";
+            Console.WriteLine(startLine);
+            Console.WriteLine(emptyLine);
+            Console.WriteLine(emptyLine);
+            Console.WriteLine("|    My name is     |");
+            Console.WriteLine("|       " + name + "       |");
+            Console.WriteLine(emptyLine);
+            Console.WriteLine(emptyLine);
+            Console.WriteLine(startLine);
+            Console.WriteLine();
+        }
+    }
+}
